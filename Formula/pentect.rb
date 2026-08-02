e27c623fadaf5b9fcc667606f75166e768e6c1ba
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2026-08-02 13:08:27 +0900

    fix: select binaries with supported Homebrew DSL

--- Formula/pentect.rb
+++ Formula/pentect.rb
@@ -5,18 +5,21 @@ class Pentect < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/EdamAme-x/pentect/releases/download/v0.0.14/pentect-macos-aarch64"
       sha256 "dfbdf9b3474c88aa96fcd8c4f6e6e4b735a338ef86428275c5c3658476ab5ed8"
-    else
+    end
+    on_intel do
       url "https://github.com/EdamAme-x/pentect/releases/download/v0.0.14/pentect-macos-x86_64"
       sha256 "c1276893f544eb355140553ba80ead8eacc228bd49cb6efa262cf870b790e43f"
     end
   end
 
   on_linux do
-    url "https://github.com/EdamAme-x/pentect/releases/download/v0.0.14/pentect-linux-x86_64"
-    sha256 "3cce25b1d9aee543e8bad8b7c217d0685f4bcb50f3de865cb8f70dd003785c37"
+    on_intel do
+      url "https://github.com/EdamAme-x/pentect/releases/download/v0.0.14/pentect-linux-x86_64"
+      sha256 "3cce25b1d9aee543e8bad8b7c217d0685f4bcb50f3de865cb8f70dd003785c37"
+    end
   end
 
   def install
